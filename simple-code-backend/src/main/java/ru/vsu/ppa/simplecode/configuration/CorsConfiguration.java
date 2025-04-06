19d6fc76a7725ebd41cd2e6f18004f93e2f11653
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2025-04-06 17:33:25 +0300

    fix: add max size configuration for Tomcat and CORS configuration

---
+++ simple-code-backend/src/main/java/ru/vsu/ppa/simplecode/configuration/CorsConfiguration.java
@@ -0,0 +1,18 @@
+package ru.vsu.ppa.simplecode.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Разрешить все источники (для разработки)
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+}
